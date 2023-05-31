79475313939514db3b70a9150611c25ff4b9a39f
Andrei Chabatul <andreichabatul@gmail.com>
2023-05-31 10:38:40 +0300

    fex item slider

--- weather/src/app/main-page/components/item-info-slider/item-info-slider.component.ts
+++ weather/src/app/main-page/components/item-info-slider/item-info-slider.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription, mergeMap } from 'rxjs';
+import { Observable, Subscription, mergeMap, switchMap } from 'rxjs';
 import { GeocodingApiService } from 'src/app/core/services/geocoding-api.service';
 import { OpenweathermapApiService } from 'src/app/core/services/openweathermap-api.service';
 import { IOpenweathermap } from 'src/app/store/models/openweathermap';
@@ -11,23 +11,54 @@ import { IOpenweathermap } from 'src/app/store/models/openweathermap';
 })
 export class ItemInfoSliderComponent implements OnInit {
 
-  cityName: string = "Москва";
-  weatherInfo$: Observable<IOpenweathermap>;
-  weatherInfo: IOpenweathermap;
+  cityName: string = "Новосибирск";
+  private querySubscription: Subscription;
+  weatherCity: IOpenweathermap;
+
 
   constructor(
+    private geocodingApiService: GeocodingApiService,
     private openweathermapApiService: OpenweathermapApiService,
-    private geocodingApiService: GeocodingApiService
   ) { }
 
   ngOnInit(): void {
-    this.weatherInfo$ = this.geocodingApiService.getCoordinate(this.cityName).pipe(
-      mergeMap(coor => this.openweathermapApiService.getWeather<IOpenweathermap>(coor[0], 'weather'))
-    );
+    this.querySubscription = this.geocodingApiService.getCoordinate(this.cityName).pipe(
+      switchMap((coordinate) =>
+        this.openweathermapApiService.getWeather<IOpenweathermap>(coordinate[0], 'weather'))
+    ).subscribe(value => {
+      this.weatherCity = value;
+    });
+
   }
+
+  ngOnDestroy(): void {
+    this.querySubscription.unsubscribe();
+  }
+
+
+
+  // weatherInfo$: Subscription;
+  // temp: number;
+  // weatherType: number;
+
+  // constructor(
+  //   private openweathermapApiService: OpenweathermapApiService,
+  //   private geocodingApiService: GeocodingApiService
+  // ) { }
+
+  // ngOnInit(): void {
+  //   this.weatherInfo$ = this.geocodingApiService.getCoordinate(this.cityName).pipe(
+  //     switchMap(coor => this.openweathermapApiService.getWeather<IOpenweathermap>(coor[0], 'weather'))
+  //   ).subscribe(value => {
+  //     this.temp = value.main.temp;
+  //     this.weatherType = value.weather[0].id;
+  //   });
+  // }
 
   // ngOnDestroy(): void {
   //   this.weatherInfo$.unsubscribe();
   // }
 
 }
+
+
